aec5fc400dc68cece17967175e8e20968cb5aaa7
David B. Mitchell <davidbmitchell77@outlook.com>
2024-07-16 13:50:12 -0700

    Update helloWorld.test.js

--- helloWorld.test.js
+++ helloWorld.test.js
@@ -23,39 +23,32 @@
 
 import { createElement } from 'lwc';
 import HelloWorld from 'c/helloWorld';
-
 describe('c-hello-world test suite', () => {
-
     beforeEach(() => {
         const element = createElement('c-hello-world', {
             is: HelloWorld
         });
         document.body.appendChild(element);
     });
-
     afterEach(() => {
         while(document.body.firstChild) {
             document.body.remove(document.body.firstChild);
         }
     });
-
     test('1 + 1 equals 2', () => {
         let n = 1 + 1;
         expect(n).toBe(2);
     });
-
     test('text equals \'Hello, world!\'', () => {
         const element = document.querySelector('div')[0];
         const div = element.shadowRoot('div');
         expect(div.textContent).toBe('Hello, world!');
     });
-
     test('text not equals \'Hello, Joe!\'', () => {
         const element = document.querySelector('div')[0];
         const div = element.shadowRoot('div');
         expect(div.textContent).not.toBe('Hello, Joe, what do you know?!');
     });
-
     test('validate input', () => {
         const element = document.querySelector('c-hello-world');
         const input = element.shadowRoot('lightning-input');
