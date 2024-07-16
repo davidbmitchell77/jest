e39e43021dd30c1b3bbf3a069822aaa53380a5f9
David B. Mitchell <davidbmitchell77@outlook.com>
2024-07-16 14:08:19 -0700

    Update myNestedComponent.test.js

--- myNestedComponent.test.js
+++ myNestedComponent.test.js
@@ -1,7 +1,7 @@
 <template>
     <lightning-card title="Nested Component Demo" icon-name="custom:custom57">
         <div class="slds-var-m-around_medium">
-            <c-child class="slds-show slds-is-relative" user-detail={userData}></c-child>
+            <c-child-component class="slds-show slds-is-relative" user-detail={userData}></c-child-component>
         </div>
     </lightning-card>
 </template>
