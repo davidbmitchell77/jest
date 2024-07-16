d0973c6f213545267a7bf8d63c7c56412880ca64
David B. Mitchell <davidbmitchell77@outlook.com>
2024-07-16 13:20:58 -0700

    Update myLoopingComponent.js

--- myLoopingComponent.js
+++ myLoopingComponent.js
@@ -1,3 +1,21 @@
+<template>
+    <lightning-card title="User List" icon:name="standard:user">
+        <div class="slds-var-m-around_medium">
+            <div class="forEach" for:each={userDetails} for:item="u">
+                <li key={id}>{u.name}</li>
+            </div>
+        </div>
+            <div class="iterator" iterator:user={userDetails}>
+                <li key={user.value.id}>
+                    <div class="slds-text-heading_large" if:true={user.first}>BEGIN LIST</div>
+                    <div class="slds-text-body">{user.value.name}</div>
+                    <div class="slds-text-heading_large" if:true={user.last}>END LIST</div>
+                </li>
+            </div>
+        </div>
+    </lightning-card>
+</template>
+
 import { createElement } from 'lwc';
 import MyLoopingComponent from 'c/my-looping-component';
 
@@ -30,27 +48,6 @@ describe('my-looping-component test suite', () => {
         expect(divLast.textContent ).toBe('END LIST'  );
     });
 });
-
-
-/*
-<template>
-    <lightning-card title="User List" icon:name="standard:user">
-        <div class="slds-var-m-around_medium">
-            <div class="forEach" for:each={userDetails} for:item="u">
-                <li key={id}>{u.name}</li>
-            </div>
-        </div>
-            <div class="iterator" iterator:user={userDetails}>
-                <li key={user.value.id}>
-                    <div class="slds-text-heading_large" if:true={user.first}>BEGIN LIST</div>
-                    <div class="slds-text-body">{user.value.name}</div>
-                    <div class="slds-text-heading_large" if:true={user.last}>END LIST</div>
-                </li>
-            </div>
-        </div>
-    </lightning-card>
-</template>
-*/
 
 
 import { LightningElement } from 'lwc';
