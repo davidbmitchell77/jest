e7bd8cc742b39bdbbf0a673912174b056c613a4c
David B. Mitchell <davidbmitchell77@outlook.com>
2024-07-16 13:22:48 -0700

    Update myConditionalRendering.test.js

--- myConditionalRendering.test.js
+++ myConditionalRendering.test.js
@@ -1,3 +1,20 @@
+<template>
+    <lightning-card title="My Conditional Rendering" icon-name="custom:custom14">
+        <div class="slds-var-m-around_medium">
+            <lightning-input> type="checkbox" label="Show password" onchange={handleChange}></lightning-input>
+        </div>
+        <div class="slds-var-m-vertical_medium userinfo">
+            <template if:false={isVisible}>
+                <p>'My password is: ***********'</p>
+            </template>
+            <template if:true={isVisible}>
+                <p>'My password is: foobar22'</p>
+            </template>
+        </div>
+    </lightning-card>
+</template>
+
+                    
 import { createElement } from 'lwc';
 import MyConditionalRendering from 'c/myConditionalRendering';
 
@@ -29,25 +46,7 @@ describe('c-my-conditional-rendering', () => {
     });
 });
 
-/*
-<template>
-    <lightning-card title="My Conditional Rendering" icon-name="custom:custom14">
-        <div class="slds-var-m-around_medium">
-            <lightning-input> type="checkbox" label="Show password" onchange={handleChange}></lightning-input>
-        </div>
-        <div class="slds-var-m-vertical_medium userinfo">
-            <template if:false={isVisible}>
-                <p>'My password is: ***********'</p>
-            </template>
-            <template if:true={isVisible}>
-                <p>'My password is: foobar22'</p>
-            </template>
-        </div>
-    </lightning-card>
-</template>
-*/
 
-/*
 import { LightningElement } from 'lwc';
 export default class MyConditionalRendering extends LightningElement {
     isVisible = false;
@@ -57,4 +56,3 @@ export default class MyConditionalRendering extends LightningElement {
         }
     }
 }
-*/
