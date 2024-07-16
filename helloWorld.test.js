8a5c1b3e7591d329c4d43b16ae23514c7ff8408c
David B. Mitchell <davidbmitchell77@outlook.com>
2024-07-16 13:37:44 -0700

    Update helloWorld.test.js

--- helloWorld.test.js
+++ helloWorld.test.js
@@ -55,6 +55,5 @@ describe('c-hello-world test suite', () => {
 
 
 import { LightningElement } from 'lwc';
-import HellowWorld from 'c/helloWorld';
 export default class HelloWorld extends LightningElement {
 }
