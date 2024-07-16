b3168519b107d9a9f7907c45eee63412d027c974
David B. Mitchell <davidbmitchell77@outlook.com>
2024-07-16 13:49:22 -0700

    Update helloWorld.test.js

--- helloWorld.test.js
+++ helloWorld.test.js
@@ -69,6 +69,23 @@ describe('c-hello-world test suite', () => {
 });
 
 
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
 export default class HelloWorld extends LightningElement {
+    fullname = "Zero to Hero";
+    title = "LWC";
+    num1 = 0;
+    num2 = 0;
+    @track address = {
+        city: "Chula Vista",
+        state: "California",
+        zip: "91913",
+        country: "United States"
+    }
+    changeHandler(event) { this.title = event.target.value;                                     }
+    trackHandler(event ) { this.address.city = event.target.value;                              }
+    num1Handler(event  ) { this.num1 = ((event.target.value != null) ? event.target.value : 0); }
+    num2Handler(event  ) { this.num2 = ((event.target.value != null) ? event.target.value : 0); }
+    get result() {
+        return (this.num1 ** this.num2);
+    }
 }
